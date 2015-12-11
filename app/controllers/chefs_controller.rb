4f2cfc9461891d96e8173e01f7db909a3accef36
Oleg <darelvc@gmail.com>
2015-12-11 13:02:52 +0200

    Add log_in and log_out pages

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,9 @@
 class ChefsController < ApplicationController
 
+	def index
+		@chefs = Chef.paginate(page: params[:id], per_page: 3)
+	end
+
 	def new
 		@chef = Chef.new
 	end
@@ -30,6 +34,7 @@ class ChefsController < ApplicationController
 
 	def show
 		@chef = Chef.find(params[:id])
+		@recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
 	end
 
 	private
