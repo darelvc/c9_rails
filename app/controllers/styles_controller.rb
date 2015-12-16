0f2fe72f75e476fcdba81158f07ccd9192697691
Oleg <darelvc@gmail.com>
2015-12-16 09:43:09 +0200

    Add styles checkboxes for new recipe

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,4 +1,6 @@
 class StylesController < ApplicationController
+	before_action :require_user, except: [:show]
+	#before_action :require_same_user, only: [:edit, :update]
 
 	def show
 		@style = Style.find(params[:id])
